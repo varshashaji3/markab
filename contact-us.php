b60edfab95cbdbaca29b2f3d57a53c2d7663cefe
varshashaji3 <77163931+varshashaji3@users.noreply.github.com>
2023-11-20 11:38:17 +0530

    Add files via upload

---
+++ contact-us.php
@@ -0,0 +1,383 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="assets/bootstarp/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/aos.css" />
+    <link rel="stylesheet" href="assets/css/super-classes.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/mobile.css" />
+
+    <link rel="icon" href="/assets/image/bg-img/19.jpeg" />
+    <title>Markab Capital | Contact Us</title>
+
+    <style>
+      .sub-banner {
+        background: linear-gradient(
+            rgba(0, 0, 0, 0.65),
+            rgba(0, 0, 0, 0.65),
+            rgba(0, 0, 0, 0.65),
+            rgba(0, 0, 0, 0.65)
+          ),
+          url(assets/image/bg-img/23.jpg) no-repeat center;
+      }
+    </style>
+  </head>
+  <body>
+    <!--navbar-sec-->
+    <header id="header-con" class="w-100 float-left">
+      <div class="wrapper">
+        <nav class="navbar navbar-expand-lg navbar-light pl-0 pr-0 pb-0">
+          <a class="navbar-brand p-0" href="index.html">
+            <figure class="mb-0">
+              <img
+                src="assets/image/core-img/logo2.png"
+                style="max-height: 90px; margin-left: 20px; width: auto"
+                alt="navbar-icon"
+              />
+            </figure>
+          </a>
+          <button
+            class="navbar-toggler collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              <li class="nav-item pl-0">
+                <a class="nav-link p-0" href="index.html"
+                  >Home <span class="sr-only">(current)</span></a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link p-0" href="about-us.html">About us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link p-0" href="news.html">News</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle p-0"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Services
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="family-office.html"
+                    >Family Office</a
+                  >
+                  <a class="dropdown-item" href="private-equity.html"
+                    >Private Equity</a
+                  >
+                  <a class="dropdown-item" href="real-estate.html"
+                    >Real Estate</a
+                  >
+                  <a class="dropdown-item" href="advisory.html">Advisory</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link p-0 is-active" href="contact.html"
+                  >Contact Us</a
+                >
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!--navbar-sec-->
+
+    <section class="sub-banner w-100 float-left shape-left shape-right d-table">
+      <div class="d-table-cell align-middle">
+        <div class="container">
+          <div class="banner-sec position-relative text-center">
+            <h1
+              class="d-inline-block position-relative"
+              data-aos="fade-down"
+              data-aos-duration="700"
+            >
+              Contact Us
+            </h1>
+            <br /><br />
+            <div
+              class="banner-button d-inline-block"
+              data-aos="fade-up"
+              data-aos-duration="700"
+            >
+              <a class="d-inline-block" href="index.html">Home</a>
+              <span>-</span>
+              <a class="d-inline-block" href="contact-us.php">Contact</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="top-button position-relative">
+        <a id="button" href="#" class="show"></a>
+      </div>
+    </section>
+
+    <!--form-sec-->
+    <div
+      class="form-section w-100 float-left padding-top padding-bottom position-relative"
+    >
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-12">
+            <div class="row justify-content-center">
+              <div class="col-md-6">
+                <h3 class="heading mb-4">Let's talk about everything!</h3>
+                <p>Contact Us, Strictly By Appointment Only</p>
+                <br />
+                <br />
+                <p>
+                  <img
+                    src="assets/image/bg-img/undraw-contact.svg"
+                    alt="Image"
+                    class="img-fluid"
+                  />
+                </p>
+              </div>
+              <div class="col-md-6">
+                <p
+                  style="font-size: 20px"
+                  class="wow fadeInUp"
+                  data-wow-delay="300ms"
+                >
+                  <i class="fa fa-phone" style="color: #ce5a67"></i> KUWAIT:
+                  +965 2220-7048
+                </p>
+                <p
+                  style="font-size: 20px"
+                  class="wow fadeInUp"
+                  data-wow-delay="400ms"
+                >
+                  <i class="fa fa-phone" style="color: #ce5a67"></i>
+                  UK: +44 (0)20-3868-6144
+                </p>
+                <br />
+
+                <form
+                  class="mb-5"
+                  method="post"
+                  action="send-email.php"
+                  id="contactForm"
+                  name="contactForm"
+                >
+                  <div class="row">
+                    <div class="col-md-6 form-group">
+                      <input
+                        type="text"
+                        class="form-control"
+                        name="fname"
+                        id="fname"
+                        placeholder="Your first name"
+                      />
+                    </div>
+                    <div class="col-md-6 form-group">
+                      <input
+                        type="text"
+                        class="form-control"
+                        name="lname"
+                        id="lname"
+                        placeholder="Your last name"
+                      />
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-md-12 form-group">
+                      <input
+                        type="text"
+                        class="form-control"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                      />
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12 form-group">
+                      <input
+                        type="text"
+                        class="form-control"
+                        name="mobile"
+                        id="mobile"
+                        placeholder="Your mobile number"
+                      />
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-md-12 form-group">
+                      <input
+                        type="text"
+                        class="form-control"
+                        name="name"
+                        id="name"
+                        placeholder="Your Country"
+                      />
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-md-12 form-group">
+                      <textarea
+                        style="width: 100%; height: 200px"
+                        class="form-control"
+                        name="message"
+                        id="message"
+                        placeholder="Write your message"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <input
+                        type="submit"
+                        value="Send Message"
+                        class="btn btn-primary rounded-0 py-2 px-4"
+                      />
+                      <span class="submitting"></span>
+                    </div>
+                  </div>
+                </form>
+                <br /><br /><br /><br /><br /><br />
+                <!-- <div id="form-message-warning mt-4"></div> 
+              <div id="form-message-success">
+                Your message was sent, thank you!
+              </div> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--form-sec-->
+
+    <!--footer-sec-->
+    <div
+      class="footer-main-con w-100 float-left padding-bottom padding-top position-relative"
+    >
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-1"></div>
+          <div class="col-lg-3 pl-0 pr-0 col-md-3">
+            <div
+              class="footer-details"
+              data-aos="fade-up"
+              data-aos-duration="700"
+            >
+              <figure>
+                <img src="assets/image/core-img/logo2.png" alt="footer-img" />
+              </figure>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-3">
+            <div
+              class="footer-links"
+              data-aos="fade-down"
+              data-aos-duration="700"
+            >
+              <h4>Useful Links</h4>
+              <ul class="list-unstyled">
+                <li class="position-relative">
+                  <a class="d-inline-block" href="about-us.html">About us</a>
+                </li>
+                <li class="position-relative">
+                  <a class="d-inline-block" href="news.html">News</a>
+                </li>
+                <li class="position-relative">
+                  <a class="d-inline-block" href="contact-us.php"
+                    >Contact us</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-lg-2 pl-lg-0 col-md-3">
+            <div
+              class="footer-social-icons"
+              data-aos="fade-down"
+              data-aos-duration="700"
+            >
+              <ul class="d-inline-block mb-0 list-unstyled">
+                <li class="d-inline-block">
+                  <a href="#"
+                    ><i
+                      class="fab fa-facebook-f d-flex align-items-center justify-content-center"
+                    ></i
+                  ></a>
+                </li>
+                <li class="d-inline-block">
+                  <a href="#"
+                    ><i
+                      class="fab fa-twitter d-flex align-items-center justify-content-center ml-0 mr-0"
+                    ></i
+                  ></a>
+                </li>
+                <li class="d-inline-block">
+                  <a href="#"
+                    ><i
+                      class="fab fa-linkedin-in d-flex align-items-center justify-content-center mb-0"
+                    ></i
+                  ></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-1"></div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-text w-100 float-left text-center">
+      <span class="d-inline-block"
+        >Copyright 2023,MarkAB Capital All Rights Reserved.</span
+      >
+    </div>
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+    <script>
+      var btn = $("#button");
+      $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+          btn.addClass("show");
+        } else {
+          btn.removeClass("show");
+        }
+      });
+
+      btn.on("click", function (e) {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "300");
+      });
+    </script>
+  </body>
+</html>
---
+++ send-email.php
@@ -0,0 +1,7 @@
+<?php
+$name=$_POST['fname'];
+$email=$_POST['email'];
+$mobile=$_POST['mobile'];
+$cname=$_POST['name'];
+$message=$_POST['message'];
+?>
